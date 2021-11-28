dd658d7c86053bd7020df62ff7ffe2f058b014bb
krzys938 <krzys938@gmial.com>
2021-11-28 16:49:39 +0100

    Odkrwanie wielu pól

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -67,7 +67,7 @@ public class Main {
     public static void LosowanieMin(int x, int y) {
         // x, y - wspolrzedne miejsca w ktore sie kliknie zamienione odpowiednio
         int licznik = 0;
-        Random rand = new Random(1);
+        Random rand = new Random();
         int tempx = 0;
         int tempy = 0;
         while (licznik < iloscMin) {
